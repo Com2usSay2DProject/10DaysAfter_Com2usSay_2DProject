cd56f3560f0b9e3d5a9211055d30fe5ed270a9cc
skku_13\skku13 <k747747@naver.com>
2025-04-10 17:56:58 +0900

    0410 / 옵션 스크롤 바 설정 -> 김형진

---
+++ Assets/02. Scripts/UI/TitleManager.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class TitleManager : MonoBehaviour
+{
+    void Start()
+    {
+        SoundManager.Instance.PlayBgm(EBgmType.Title);
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
